7c3e807a0b133303c720f561fe8d72d9d897479c
Robert Joseph Saltos <Rsaltos@csustan.edu>
2025-05-12 11:20:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ AOD_min = st.sidebar.slider("Y-Axis Min", min_value=0.0, max_value=1.0, value=0.
 AOD_max = st.sidebar.slider("Y-Axis Max", min_value=0.0, max_value=1.0, value=0.3, step=0.01)
 
 # Input GitHub URL for the first repository
-file_url_1 = "https://raw.githubusercontent.com/Rsaltos7/TurlockAOD2024/refs/heads/main/20240101_20241231_Turlock_CA_USA.lev15"
+file_url_1 = "https://github.com/Rsaltos7/Kincade-Fire-/blob/main/20190101_20190101_Modesto.siz"
 
 # Function to load data from the given URL
 def load_data(file_url):
